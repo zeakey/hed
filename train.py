379507c98758d56080b6a16313a64193fd4a9289
KAI ZHAO <zhaok1206@gmail.com>
2018-05-14 16:06:41 +0800

    update

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ from __future__ import division
 import numpy as np
 import sys, os, argparse
 from os.path import isfile, join, isdir
-sys.path.insert(0, 'caffe/python')
 sys.path.insert(0, 'model')
 sys.path.insert(0, 'lib')
 import caffe
@@ -10,7 +9,9 @@ parser = argparse.ArgumentParser(description='Training hed.')
 parser.add_argument('--gpu', type=int, help='gpu ID', default=0)
 parser.add_argument('--solver', type=str, help='solver', default='model/hed_solver.pt')
 parser.add_argument('--weights', type=str, help='base model', default='model/vgg16convs.caffemodel')
+parser.add_argument('--caffe', type=str, help='base model', default='caffe')
 args = parser.parse_args()
+sys.path.insert(0, join(args.caffe, 'python'))
 assert isfile(args.weights) and isfile(args.solver)
 caffe.set_mode_gpu()
 caffe.set_device(args.gpu)
